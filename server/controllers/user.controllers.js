ce9e14b163cb68baabc07f0f7aa0bec00880a230
DesireeBesnard <desireebesnard@gmail.com>
2022-08-24 16:48:37 +0200

    feat:(user) update user

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import UserModel from "../models/user.js";
+import bcrypt from "bcrypt"
 
 export const getUser = async(req, res) => {
     const id = req.params.id
@@ -13,4 +14,25 @@ export const getUser = async(req, res) => {
     } catch (error) {
         res.status(500).json(error)
     }
+}
+
+export const updateUser = async(req, res) => {
+    const id = req.params.id
+    const { currentUserId, currentUserAdminStatus, password } = req.body
+
+    if(id === currentUserId || currentUserAdminStatus) {
+        try {
+
+            if(password) {
+                const salt = await bcrypt.genSalt(10)
+                req.body.password = await bcrypt.hash(password, salt)
+            }
+
+            const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true})
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+
 }
