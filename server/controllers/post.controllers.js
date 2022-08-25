74bfb7353f7c11006df9bd0192698a85291d8203
DesireeBesnard <desireebesnard@gmail.com>
2022-08-25 20:13:33 +0200

    feat(posts): get timeline

--- server/controllers/post.controllers.js
+++ server/controllers/post.controllers.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import postModel from "../models/post.js";
+import UserModel from "../models/user.js";
 
 export const createPost = async(req, res) => {
     const newPost = new postModel(req.body)
@@ -59,4 +60,60 @@ export const deletePost = async(req, res) => {
     } catch (error) {
         res.status(500).json(error)
     }
+}
+
+export const likePost = async(req, res) => {
+    const id = req.params.id
+    const {userId} = req.body
+
+    try {
+        const post = await postModel.findById(id)
+        if(!post.likes.includes(userId)) {
+            await post.updateOne({$push: {likes: userId}})
+            res.status(200).json("Post liked")
+        } else {
+            await post.updateOne({$pull: {likes: userId}})
+            res.status(200).json("Post unliked")
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+//get Timeline posts
+export const getTimeLinePosts = async(req, res) => {
+    const userId = req.params.id
+
+    try {
+        const currentUserPosts = await postModel.find({userId: userId})
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match: {
+                    _id : new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "posts",
+                    localField: "following",
+                    foreignField: "userId",
+                    as: "followingPosts"
+                }
+            },
+            {
+                $project: {
+                    followingPosts: 1,
+                    _id: 0
+                }
+            }
+        ])
+        res.status(200).json(currentUserPosts.concat(...followingPosts[0].followingPosts).sort((a,b)=> {
+            return b.createdAt - a.createdAt
+        }))
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
 }
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { createPost, deletePost, getPost, updatePost } from "../controllers/post.controllers.js"
+import { createPost, deletePost, getPost, getTimeLinePosts, likePost, updatePost } from "../controllers/post.controllers.js"
 
 const router = express.Router()
 
 router.post('/', createPost)
 router.get('/:id', getPost)
+router.get('/:id/timeline', getTimeLinePosts)
 router.put('/:id', updatePost)
+router.put('/:id/like', likePost)
 router.delete('/:id', deletePost)
 
 export default router
