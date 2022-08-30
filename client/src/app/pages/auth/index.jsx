fee3dcd4b454106a627d6aa472e3ba5f61b9ba24
DesireeBesnard <desireebesnard@gmail.com>
2022-08-30 09:21:22 +0200

    feat (auth page): form validation

--- client/src/app/pages/auth/index.jsx
+++ client/src/app/pages/auth/index.jsx
@@ -2,37 +2,44 @@ import { useState } from "react"
 import "./style.css"
 
 
-function SignUp() {
-    return (
-        <div className="a-right">
-            <form className="info-form auth-form" action="">
-
-                <h3>Sign up</h3>
-
-                <div>
-                    <input className="info-input" type="text" placeholder="First name" name="Firstname" />
-                    <input className="info-input" type="text" placeholder="Last name" name="Lastname" />
-                </div>
-                <div>
-                    <input className="info-input" type="text" name="username" placeholder="Username" />
-                </div>
-                <div>
-                    <input className="info-input" type="password" name="password" placeholder="Password" />
-                    <input className="info-input" type="password" name="confirm" placeholder="Confirm password" />
-                </div>
-
-                <div>
-                    <span className="account">Already have an account. Login!</span>
-                </div>
-
-                <button className="button info-button" type="submit">Signup</button>
-            </form>
-        </div>
-    )
-}
-
 const Auth = () => {
     const [isSignUp, setIsSignUp] = useState(false)
+    const [data, setData] = useState({
+        firstName:"", lastName:"", userName:"", password:"", confirm:""
+    })
+    const [userNameFormat, setUserNameFormat] = useState(true)
+    const [confirmPassword, setConfirmPassword] = useState(true)
+
+    const validEmail = new RegExp('[a-z0-9]+@[a-z]+\\.[a-z]{2,3}')
+
+    const handleChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit =(e) => {
+        e.preventDefault()
+    
+        if(isSignUp) {
+
+            if(!validEmail.test(data.userName)) {
+                setUserNameFormat(false)
+            } else {
+                setUserNameFormat(true)
+            }
+            if (data.password !== data.confirm) {
+                setConfirmPassword(false)
+            } else {
+                setConfirmPassword(true)
+            }
+        }
+    }
+
+    const resetForm = () => {
+        setUserNameFormat(true)
+        setConfirmPassword(true)
+        setData({firstName:"", lastName:"", userName:"", password:"", confirm:""})
+    }
+
     return (
         <div className="auth">
             {/*Left Side */}
@@ -44,35 +51,60 @@ const Auth = () => {
                 </div>
             </div>
 
-            {/*<SignUp />*/}
-
             {/*Right Side */}
             <div className="a-right">
-            <form className="info-form auth-form" action="">
+            <form className="info-form auth-form" action="" onSubmit={handleSubmit}>
 
                 <h3>{isSignUp ? "Sign up" : "Login"}</h3>
 
                 {isSignUp && (
                     <div>
-                        <input className="info-input" type="text" placeholder="First name" name="Firstname" />
-                        <input className="info-input" type="text" placeholder="Last name" name="Lastname" />
+                        <input className="info-input" type="text" placeholder="First name" name="firstName" value={data.firstName} onChange={handleChange} />
+                        <input className="info-input" type="text" placeholder="Last name" name="lastName" value={data.lastName} onChange={handleChange} />
                     </div>
 
                 )}
 
                 <div>
-                    <input className="info-input" type="text" name="username" placeholder="Username" />
+                    <input className="info-input" type="text" name="userName" placeholder="Username" value={data.userName} onChange={handleChange} />
                 </div>
                 <div>
-                    <input className="info-input" type="password" name="password" placeholder="Password" />
+                    <input className="info-input" type="password" name="password" placeholder="Password" value={data.password}  onChange={handleChange} />
                     {isSignUp && (
-                        <input className="info-input" type="password" name="confirm" placeholder="Confirm password" />
+                        <input className="info-input" type="password" name="confirm" placeholder="Confirm password" value={data.confirm} onChange={handleChange} />
                     )}
 
                 </div>
 
+                {isSignUp && (
+                    userNameFormat? "" : 
+                        <>
+                            <div>
+                                <span>
+                                    * Username invalid format
+                                </span>
+                            </div>                        
+                        </>
+                )}
+                {isSignUp && (
+    
+                    confirmPassword? "" :
+                        <>
+                            <div>
+                                <span>
+                                    * Confirm password is not same
+                                </span>
+                            </div>
+                        </>
+                )}
+
+
+
+
                 <div>
-                    <span className="account" onClick={() => setIsSignUp(prev => !prev)}>{isSignUp ? "Already have an account. Login!" : "Don't have an account. Sign Up"}</span>
+                    <span className="account" onClick={() => {setIsSignUp(prev => !prev); resetForm()}}>
+                        {isSignUp ? "Already have an account. Login!" : "Don't have an account. Sign Up"}
+                    </span>
                 </div>
 
                 <button className="button info-button" type="submit">{ isSignUp ? "Signup" : "Log In"}</button>
